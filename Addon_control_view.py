c251bfdd06a5b2099408c2200c003e1718180ab3
HoloAdventure <dezi_g@hotmail.co.jp>
2022-02-21 23:16:34 +0900

    シェーディングタイプの切り替えボタンを追加

--- Addon_control_view.py
+++ Addon_control_view.py
@@ -5,7 +5,7 @@ ADDON_OPERATOR_IDNAME_STATUS = "holomon.show_status"
 # 利用するタイプやメソッドのインポート
 import bpy
 from bpy.types import Operator, Panel, PropertyGroup
-from bpy.props import PointerProperty
+from bpy.props import PointerProperty, EnumProperty
 
 # 継承するクラスの命名規則は以下の通り
 # [A-Z][A-Z0-9_]*_(継承クラスごとの識別子)_[A-Za-z0-9_]+
@@ -71,11 +71,22 @@ class HOLOMON_PT_holomon_control_view(Panel):
         # Operatorをボタンとして配置する
         draw_layout = self.layout
         # 要素行を作成する
-        line_row = draw_layout.row()
-        # ポリゴン数の表示切替を実行するボタンを配置する
-        line_row.operator(ADDON_OPERATOR_IDNAME_STATUS)
+        showstats_row = draw_layout.row()
+        # ポリゴン情報の表示切替を実行するボタンを配置する
+        showstats_row.operator(ADDON_OPERATOR_IDNAME_STATUS)
+        # ボックス要素を作成する
+        shading_box = draw_layout.box()
+        # 要素行を作成する
+        shading_row = shading_box.row()
+        # テキストを配置する
+        shading_row.label(text="シェーディング切替")
+        # 要素列を作成する
+        shading_column = shading_box.column(align=True)
+        # シェーディングタイプの選択肢を配置する
+        # row(列)要素に expand=True を指定して EnumProperty を設定すると縦並びに表示される
+        shading_column.prop(context.scene.holomon_control_view, "prop_shadingselect", expand=True)
 
-# Operatorクラスの作成（ポリゴン数表示のためのサブ関数）
+# Operatorクラスの作成（ポリゴン情報表示のためのサブ関数）
 # 参考URL:https://docs.blender.org/api/current/bpy.types.Operator.html
 class HOLOMON_OT_holomon_control_view(Operator):
     # クラスのIDを定義する
@@ -83,7 +94,7 @@ class HOLOMON_OT_holomon_control_view(Operator):
     bl_idname = ADDON_OPERATOR_IDNAME_STATUS
     # クラスのラベルを定義する
     # (デフォルトのテキスト表示などに利用)
-    bl_label = "統計情報の表示切替"
+    bl_label = "ポリゴン情報の表示切替"
     # クラスの説明文
     # (マウスオーバー時に表示)
     bl_description = "3Dビューの統計データ表示を切り替えてオブジェクト数やポリゴン数を表示します"
@@ -116,12 +127,45 @@ class HOLOMON_OT_holomon_control_view(Operator):
         
         return {'FINISHED'}
 
+# PropertyGroupクラスの作成
+# 参考URL:https://docs.blender.org/api/current/bpy.types.PropertyGroup.html
+class HOLOMON_PROP_holomon_control_view(PropertyGroup):
+    # 選択肢の更新時に実行する関数を定義する
+    def change_shadingselect(self, context):
+        # 指定された選択肢を取得する
+        target_shadingselect = context.scene.holomon_control_view.prop_shadingselect
+        # 選択したIDを元にシェーディングタイプを設定する
+        # (https://docs.blender.org/api/current/bpy.types.View3DShading.html#bpy.types.View3DShading.type)
+        if target_shadingselect == "Wireframe":
+            context.space_data.shading.type = 'WIREFRAME'
+        elif target_shadingselect == "Solid":
+            context.space_data.shading.type = 'SOLID'
+        elif target_shadingselect == "Material":
+            context.space_data.shading.type = 'MATERIAL'
+
+    # シーン上のパネルに表示する選択肢のカスタムプロパティを定義する
+    # 選択肢(ID,名前,説明)を作成する
+    # EnumPropertyの選択肢要素のマニュアル
+    # (https://docs.blender.org/api/current/bpy.props.html#bpy.props.EnumProperty)
+    prop_shadingselect: EnumProperty(
+        name="シェーディングの選択",                                     # プロパティ名
+        items= [
+            ("Wireframe","ワイヤーフレーム表示","ワイヤーフレーム表示に切り替え","SHADING_WIRE",0),
+            ("Solid","ソリッド表示","ソリッド表示に切り替え","SHADING_SOLID",1),
+            ("Material","マテリアル表示","マテリアル表示に切り替え","SHADING_TEXTURE",2),
+        ],                                                               # 選択肢
+        default="Solid",                                                 # デフォルト値
+        description = "3Dビューのシェーディング処理を指定します",          # 説明文
+        update = change_shadingselect,                                  # 更新時実行関数
+        )
+
 
 # 登録に関する処理
 # 登録対象のクラス名
 regist_classes = (
     HOLOMON_PT_holomon_control_view,
     HOLOMON_OT_holomon_control_view,
+    HOLOMON_PROP_holomon_control_view,
 )
 
 # 作成クラスと定義の登録メソッド
@@ -129,9 +173,15 @@ def register():
     # カスタムクラスを登録する
     for regist_cls in regist_classes:
         bpy.utils.register_class(regist_cls)
+    # シーン情報にカスタムプロパティを登録する
+    bpy.types.Scene.holomon_control_view = \
+      PointerProperty(type=HOLOMON_PROP_holomon_control_view)
 
 # 作成クラスと定義の登録解除メソッド
 def unregister():
+    # カスタムクラスを解除する
+    for regist_cls in regist_classes:
+        bpy.utils.unregister_class(regist_cls)
     # カスタムクラスを解除する
     for regist_cls in regist_classes:
         bpy.utils.unregister_class(regist_cls)
